7f8fe78726791a52d02f697424957f367ff66c68
Nahdia Putri <nptrsfr1808@gmail.com>
2025-09-10 12:06:44 +0700

    Langkah 1 dan 2

---
+++ Jobsheet2/Praktikum2.dart
@@ -0,0 +1,12 @@
+void main() {
+  int counter = 0;   // inisialisasi counter
+
+  while (counter < 33) {
+    print(counter);
+    counter++;
+  }
+}
+// penjelasan
+//Ketika dijalankan, program akan mencetak angka mulai dari 0 sampai 32.
+//Hal ini karena perulangan while terus berjalan selama kondisi counter < 33 bernilai benar. 
+//Pada saat counter sudah bernilai 33, kondisi salah dan perulangan berhenti.
