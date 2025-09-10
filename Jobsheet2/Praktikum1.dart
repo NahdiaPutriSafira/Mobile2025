bf6d7b0b894cef3c8da6d8a29d025fa855602c1f
Nahdia Putri <nptrsfr1808@gmail.com>
2025-09-10 12:02:01 +0700

    Langkah3

--- Jobsheet2/Praktikum1.dart
+++ Jobsheet2/Praktikum1.dart
@@ -9,11 +9,23 @@ void main() {
   }
 
   if (test == "test2") print("Test2 again");
-}
-// penjelasannya
+
+  // penjelasannya
 //ketika kode dijalankan, program akan mencetak output "Test2" dan "Test2 again". 
 //Hal ini terjadi karena nilai variabel test adalah "test2". 
 //Saat blok if pertama diperiksa, kondisi if (test == "test1") bernilai salah, sehingga tidak dijalankan. 
 //Program kemudian masuk ke bagian else if (test == "test2"), kondisi ini benar sehingga mencetak "Test2". 
 //Setelah itu, program masih melanjutkan ke perintah if (test == "test2") print("Test2 again");. Karena kondisi ini juga benar, maka dicetak lagi "Test2 again". Jadi, hasil akhir ada dua baris output yaitu "Test2" dan "Test2 again".
+
+  String test2 = "true";
+
+  if (test2 == "true") {
+    print("Kebenaran");
+  } else {
+    print("Bukan kebenaran");
+  }
+}
+
+
+
 
