c11ef0c5497c0f0619c6e9a6f3b47bfa15a38def
Nahdia Putri <nptrsfr1808@gmail.com>
2025-09-10 12:19:52 +0700

    Langkah 3

--- Jobsheet2/Praktikum3.dart
+++ Jobsheet2/Praktikum3.dart
@@ -1,5 +1,7 @@
 void main() {
   for (int index = 10; index < 27; index++) {
+    if (index == 21) break;
+    else if (index > 1 && index < 7) continue;
     print(index);
   }
 }
@@ -9,3 +11,8 @@ void main() {
 //Pada for (Index = 10; index < 27; index), variabel Index/index belum pernah dideklarasikan (int belum ada), jadi compiler tidak tahu tipenya.
 //Bagian index++ sudah benar, tapi karena deklarasi tidak ada, tetap error.
 //setelah diperbaiki program akan mencetak angka 10 sampai 26 secara berurutan.
+
+//Pada langkah 3, perulangan for berjalan dari 10 sampai 26. 
+//Jika index == 21, perulangan dihentikan oleh break, sehingga angka setelah 21 tidak muncul. 
+//Kondisi else if (index > 1 && index < 7) tidak berpengaruh karena perulangan dimulai dari 10. 
+//Hasil akhirnya hanya menampilkan angka 10 sampai 20.
