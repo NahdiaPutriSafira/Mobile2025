b6e509d9956a7a2cf3919fc88f7579f622785780
Nahdia Putri <nptrsfr1808@gmail.com>
2025-09-10 20:40:04 +0700

    Langkah1 dan 2

---
+++ Jobsheet3/Praktikum2.dart
@@ -0,0 +1,8 @@
+void main() {
+  var halogens = {'fluorine', 'chlorine', 'bromine', 'iodine', 'astatine'};
+  print(halogens);
+}
+//Kode ini membuat sebuah Set berisi lima elemen string. Tipe data Set mirip seperti list, tetapi tidak mengizinkan adanya duplikasi elemen dan tidak menjamin urutan. 
+//Saat dijalankan, output akan menampilkan isi set
+//Jika ditulis benar, tidak ada error. Tetapi kalau kamu menggunakan tanda kurung kurawal {} tanpa isi, Dart akan menganggapnya sebagai Map, bukan Set. 
+//Karena itu, isi harus diberikan agar dikenali sebagai Set.
