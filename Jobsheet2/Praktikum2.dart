eb68c3ab49dbf6b9ff81cda38ac591eb223bf523
Nahdia Putri <nptrsfr1808@gmail.com>
2025-09-10 12:09:44 +0700

    Langkah 3

--- Jobsheet2/Praktikum2.dart
+++ Jobsheet2/Praktikum2.dart
@@ -5,8 +5,20 @@ void main() {
     print(counter);
     counter++;
   }
+  // reset counter untuk do-while
+  counter = 33;
+
+  do {
+    print(counter);
+    counter++;
+  } while (counter < 77);
 }
-// penjelasan
+// penjelasan langkah 1 dan 2
 //Ketika dijalankan, program akan mencetak angka mulai dari 0 sampai 32.
 //Hal ini karena perulangan while terus berjalan selama kondisi counter < 33 bernilai benar. 
 //Pada saat counter sudah bernilai 33, kondisi salah dan perulangan berhenti.
+
+//penjelasan langkah 3
+//Pada do-while, blok program selalu dijalankan minimal sekali, baru dicek kondisinya.
+//Hasilnya: program mencetak angka mulai dari 33 sampai 76.
+//Jadi, jika digabung dengan while sebelumnya, output lengkap adalah angka dari 0 sampai 76.
