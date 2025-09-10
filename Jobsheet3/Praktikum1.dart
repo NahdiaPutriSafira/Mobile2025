f407666f1f188f32f9f2fa12da93f5c358c14c22
Nahdia Putri <nptrsfr1808@gmail.com>
2025-09-10 20:34:59 +0700

    Langkah3

--- Jobsheet3/Praktikum1.dart
+++ Jobsheet3/Praktikum1.dart
@@ -1,17 +1,7 @@
 void main() {
-var list = [1, 2, 3];
-assert(list.length == 3);
-assert(list[1] == 2);
-print(list.length);
-print(list[1]);
-
-list[1] = 1;
-assert(list[1] == 1);
-print(list[1]);  
+  var list = List<String?>.filled(5, null, growable: false);
+  list[1] = "Nahdia Putri Safira";
+  list[2] = "2341720015"; 
+  print(list);
 }
-
-//Pada saat kode dijalankan program pertama kali membuat sebuah list dengan isi [1, 2, 3]. 
-//Kemudian dilakukan menggunakan assert untuk memastikan bahwa panjang list benar bernilai 3 dan bahwa elemen pada indeks ke-1 adalah 2. 
-//Karena kedua kondisi ini benar, eksekusi program berlanjut tanpa error. Selanjutnya, program mencetak panjang list yaitu 3 dan nilai pada indeks ke -1 yatu 2. 
-//Setelah itu, nilai pada indeks ke-1 diubah dari 2 menjadi 1, lalu kembali dilakukan pengecekan dengan assert  bahwa nilai pada indeks tersebut memang sudah berubah menjadi 1.
-//Karena kondisi ini juga benar, program berjalan keluaran program adalah urutan angka 3, 2 dan 1
+//hasil akhirnya adalah sebuah list dengan panjang 5, di mana elemen indeks ke-1 berisi nama kamu, indeks ke-2 berisi NIM kamu, dan indeks lain tetap bernilai null.
