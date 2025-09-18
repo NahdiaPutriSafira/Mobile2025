cd10fbc7ee8106158cb3a040eec0f5eafdb607d1
Nahdia Putri <nptrsfr1808@gmail.com>
2025-09-18 12:37:37 +0700

    Langkah3

--- Jobsheet3/Praktikum3.dart
+++ Jobsheet3/Praktikum3.dart
@@ -1,7 +1,13 @@
 void main() {
   for (int index = 10; index < 27; index++) {
+    if (index == 21) break;            
+    if (index > 1 && index < 7) continue; 
     print(index);
   }
 }
 
+
 //program menampilkan urutan bilangan bulat dari 10 hingga 26.
+//pada langkah 3 di dalam perulangan ditambahkan perintah if (index == 21) break; yang berfungsi untuk menghentikan perulangan ketika nilai index sama dengan 21. 
+//Selain itu, terdapat perintah if (index > 1 && index < 7) continue; yang sebenarnya tidak berpengaruh karena perulangan dimulai dari angka 10, sehingga tidak ada nilai yang memenuhi kondisi tersebut. 
+//Dengan adanya break, program hanya mencetak angka dari 10 sampai 20, lalu berhenti saat index bernilai 21.
