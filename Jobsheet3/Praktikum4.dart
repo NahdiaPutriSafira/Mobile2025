b79fc807bac96df563273492f87cb722a70172c6
Nahdia Putri <nptrsfr1808@gmail.com>
2025-09-18 16:27:28 +0700

    Praktikum4_langkah3_penambahan NIM

--- Jobsheet3/Praktikum4.dart
+++ Jobsheet3/Praktikum4.dart
@@ -2,9 +2,14 @@ void main() {
   var list1 = [1, 2, null];
   var list2 = [0, ...?list1];
 
+   // Tambahkan list NIM
+  var nim = ['2341720015'];          // list berisi NIM
+  var list3 = [...list2, ...nim];
+
   print(list1);
   print(list2);
   print(list2.length);
+  print(list3);
 }
 
 // ...?list1 digunakan agar list tetap bisa disebarkan walaupun ada elemen null.
