f28ead2c5b8b43ee28775b8202a982ce5ea2d59e
Nahdia Putri <nptrsfr1808@gmail.com>
2025-09-18 14:56:02 +0700

    Praktikum4_Langkah3

--- Jobsheet3/Praktikum4.dart
+++ Jobsheet3/Praktikum4.dart
@@ -1,13 +1,14 @@
 void main() {
-  var list1 = [1, 2, 3];
-  var list2 = [0, ...list1];
+  var list1 = [1, 2, null];
+  var list2 = [0, ...?list1];
 
   print(list1);
   print(list2);
   print(list2.length);
 }
 
-//...list1 disebut spread operator untuk menyalin isi list1 ke dalam list2.
-// list2 diawali dengan 0, lalu ditambah isi list1 → hasilnya [0, 1, 2, 3].
-// Panjang list2 adalah 4.
-// Tidak ada error.
+// ...?list1 digunakan agar list tetap bisa disebarkan walaupun ada elemen null.
+
+// Hasilnya list2 [0, 1, 2, null] dengan panjang 4.
+
+// Jika pakai ...list1 tanpa tanda tanya, bisa error jika list1 bernilai null.
