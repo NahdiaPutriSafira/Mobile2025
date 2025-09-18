d4d20fd89faba070ef6d4ebf77a0e121794ae726
Nahdia Putri <nptrsfr1808@gmail.com>
2025-09-18 13:19:28 +0700

    Praktikum3_Langkah1_pembenaran

--- Jobsheet3/Praktikum3.dart
+++ Jobsheet3/Praktikum3.dart
@@ -1,13 +1,20 @@
 void main() {
-  for (int index = 10; index < 27; index++) {
-    if (index == 21) break;            
-    if (index > 1 && index < 7) continue; 
-    print(index);
-  }
+  var gifts = {
+    'first': 'partridge',
+    'second': 'turtledoves',
+    'fifth': 1
+  };
+
+  var nobleGases = {
+    2: 'helium',
+    10: 'neon',
+    18: 2,
+  };
+
+  print(gifts);
+  print(nobleGases);
 }
-
-
-//program menampilkan urutan bilangan bulat dari 10 hingga 26.
-//pada langkah 3 di dalam perulangan ditambahkan perintah if (index == 21) break; yang berfungsi untuk menghentikan perulangan ketika nilai index sama dengan 21. 
-//Selain itu, terdapat perintah if (index > 1 && index < 7) continue; yang sebenarnya tidak berpengaruh karena perulangan dimulai dari angka 10, sehingga tidak ada nilai yang memenuhi kondisi tersebut. 
-//Dengan adanya break, program hanya mencetak angka dari 10 sampai 20, lalu berhenti saat index bernilai 21.
+//penjelasan output
+// Variabel gifts menggunakan Map dengan key bertipe String, sedangkan value bisa campuran (String dan int).
+// Variabel nobleGases menggunakan Map dengan key bertipe int, sedangkan value juga campuran (String dan int).
+// Tidak ada error karena Dart memperbolehkan Map dengan tipe data campuran jika kita tidak menentukan tipe generic (Map<String, String>, dll).
