f5135b13ecb292d39e7b5dbda82c1ecb47da7d79
Nahdia Putri <nptrsfr1808@gmail.com>
2025-09-23 08:24:11 +0700

    Praktikum5_langkah3

--- Jobsheet3/Praktikum5.dart
+++ Jobsheet3/Praktikum5.dart
@@ -1,4 +1,11 @@
+(int, int) tukar((int, int) record) {
+  var (a, b) = record;
+  return (b, a);
+}
 void main() {
+  //pemanggilan tukar()
+   var hasil = tukar((1, 2));
+  print(hasil);
   var record = ('first', a: 2, b: true, 'last');
   print(record);
 }
