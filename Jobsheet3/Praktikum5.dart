c7d319f0f500de8073327df1290fb2ad33835038
Nahdia Putri <nptrsfr1808@gmail.com>
2025-09-23 08:25:50 +0700

    Praktikum5_langkah4

--- Jobsheet3/Praktikum5.dart
+++ Jobsheet3/Praktikum5.dart
@@ -8,4 +8,8 @@ void main() {
   print(hasil);
   var record = ('first', a: 2, b: true, 'last');
   print(record);
+
+  (String, int) mahasiswa1;
+  mahasiswa1 = ('Nahdia', 2341720015);
+  print('Langkah 4: $mahasiswa1');
 }
